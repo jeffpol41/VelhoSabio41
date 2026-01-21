b0b93fec3500a1e6490a6ee15241bd5a23de0d66
jeffpol41 <jeffersongabrieloliveira41@gmail.com>
2026-01-21 15:13:52 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: "/VelhoSabio41/", // 👈 ESSENCIAL
   server: {
     host: "::",
     port: 8080,
